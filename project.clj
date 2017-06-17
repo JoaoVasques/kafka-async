0908965325ae7cb42c47ad5ab275ecdb00771abe
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2017-06-17 16:21:17 +0100

    Add documentation plugin
    Using Codox as the documentation generation plugin

--- project.clj
+++ project.clj
@@ -7,4 +7,6 @@
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.442"]
                  [org.apache.kafka/kafka_2.11 "0.10.1.0"]
-                 ])
+                 ]
+  :plugins [[lein-codox "0.10.3"]]
+  :target-path "docs")
